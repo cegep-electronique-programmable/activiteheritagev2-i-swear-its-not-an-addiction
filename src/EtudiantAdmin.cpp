8580c68380c1e54a3fddcf56be9e80b0ece962e3
Krypthick <Krypthickgmail.com>
2025-05-08 11:10:27 -0400

    wjajofwnfa

---
+++ src/EtudiantAdmin.cpp
@@ -0,0 +1,36 @@
+/*
+ * Nom de fichier 
+ *      EtudiantAdmin.cpp
+ * 
+ * Sommaire
+ *      Fichier source pour la classe EtudiantAdmin.
+ * 
+ * Auteur 
+ *      Jeff Truong
+ *  
+ */
+#include "EtudiantAdmin.h"
+
+//Définition de la méthode setMatricule de la classe, permet de modifier le matricule de l'étudiant
+void EtudiantAdmin::setMatricule(int nouveauMatricule){
+    //Le numero de matricule doit necessairement avoir 7 chiffres et commence par 5.
+    if ((nouveauMatricule >= 5000000) && (nouveauMatricule <= 5999999)){
+        this->numeroMatricule = nouveauMatricule;    
+    }
+    else{
+        this->numeroMatricule = 0; //Le matricule est mis à 0 si le matricule n'est pas valide
+    }
+}
+
+void EtudiantAdmin::setcountSession1 (int nouvelleMoyenne){
+    if (nouvelleMoyenne <= 100){
+        this->moyenne = nouvelleMoyenne;
+    }
+    else{
+        this->moyenne = 100;
+    }
+}
+
+int EtudiantAdmin::getcountSession1(void){
+    return (this->moyenne);
+}
